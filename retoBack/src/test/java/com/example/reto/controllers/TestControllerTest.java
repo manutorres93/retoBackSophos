697a397ee1d09781018d9f229272a52f66fce95c
manutorres93 <manutorres9312@gmail.com>
2022-12-30 12:54:27 -0500

    Test de Test

---
+++ retoBack/src/test/java/com/example/reto/controllers/TestControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.reto.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
